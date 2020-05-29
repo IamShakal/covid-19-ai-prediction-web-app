8cf9794d74c678f8b5ff9e1dfbbcec933af193db
Gourab <64215693+IamShakal@users.noreply.github.com>
2020-05-29 10:54:23 +0530

    Update settings.py

--- corona_predictions/settings.py
+++ corona_predictions/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['cov19aipredictions.pythonanywhere.com', 'allpredtai.herokuapp.com']
+ALLOWED_HOSTS = ['cov19aipredictions.pythonanywhere.com', 'herokuapp.com']
 
 
 # Application definition
